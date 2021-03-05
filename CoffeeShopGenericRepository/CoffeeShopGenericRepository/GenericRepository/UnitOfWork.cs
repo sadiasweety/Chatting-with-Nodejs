dbcb3dad08120e46af5524180910f222e839888b
Sadia Islam Sweety <70953351+sadiasweety@users.noreply.github.com>
2021-03-06 00:52:38 +0600

    Add files via upload

---
+++ CoffeeShopGenericRepository/CoffeeShopGenericRepository/GenericRepository/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeShopGenericRepository.Models;
+
+namespace CoffeeShopGenericRepository.GenericRepository
+{
+    public class UnitOfWork : IDisposable
+    {
+        private CoffeeDBEntities context = new CoffeeDBEntities();
+
+        private GenericRepository<CustomerInfo> customerInfoRepository;
+
+        public GenericRepository<CustomerInfo> CustomerInfoRepository
+        {
+            get
+            {
+
+                if (this.customerInfoRepository == null)
+                {
+                    this.customerInfoRepository = new GenericRepository<CustomerInfo>(context);
+                }
+                return customerInfoRepository;
+            }
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
